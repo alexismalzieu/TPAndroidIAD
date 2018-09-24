e879a0b79fe8cddd177596239cab6992aa7e8498
alexismalzieu <alex.malzieu@gmail.com>
2018-09-24 16:09:50 +0200

    swipe to dismiss

--- app/src/main/java/com/tards/imt/mypointofinterest/PoiListActivity.java
+++ app/src/main/java/com/tards/imt/mypointofinterest/PoiListActivity.java
@@ -5,12 +5,14 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -53,6 +55,23 @@ public class PoiListActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+               mPoiList.remove(viewHolder.getAdapterPosition());
+               mAdapter.notifyDataSetChanged();
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -167,4 +186,5 @@ public class PoiListActivity extends AppCompatActivity {
         mPoiList.add(testPoi2);
         mPoiList.add(testPoi3);
     }
+
 }
